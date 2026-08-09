8f89070b6b2ae5c1606219d952233cdda7812322
clash-tutorial CI <actions@github.com>
2026-08-09 11:03:36 +0000

    Publish the book from christiaanb/clash-tutorial@43e3afab1c79c1e6344a8fb5e4d217928aa03578

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-34e33fc4.js',
+                path_to_root + 'searchindex-f44a8b9d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
